d96daaf41d361a4e11d38322ecf2860801b2c041
dito003 <noreply>
2014-10-06 19:35:11 +0000

    Add component nav

---
+++ ui/nav.reel/nav.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/nav.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Nav
+ * @extends Component
+ */
+exports.Nav = Component.specialize(/** @lends Nav# */ {
+    constructor: {
+        value: function Nav() {
+            this.super();
+        }
+    }
+});
